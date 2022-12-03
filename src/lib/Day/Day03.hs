28e4b2a00511002af25a24835ed70d55aa1b35bc
Morten Kolstad <morten.kolstad@bekk.no>
2022-12-03 10:43:01 +0100

    day 3

--- src/lib/Day/Day03.hs
+++ src/lib/Day/Day03.hs
@@ -1,22 +1,41 @@
 module Day.Day03 where
 
+import Control.Lens
+import Data.List.Extra (chunksOf)
+import Data.Map qualified as Map
+import Data.Set qualified as Set
 import Test.HUnit ((@=?))
 
-parse = id
+letters :: Map.Map Char Integer
+letters = Map.fromList $ zip (['a' .. 'z'] ++ ['A' .. 'Z']) [1 ..]
 
-solveA = id
+priority :: Char -> Integer
+priority x = letters Map.! x
 
-solveB = id
+parseBackpacksInHalves :: String -> [(Set.Set Char, Set.Set Char)]
+parseBackpacksInHalves = fmap into2 . lines
+ where
+  into2 xs = over each Set.fromList $ splitAt (length xs `div` 2) xs
+
+sumOfCommonItemPrioritiesInHalves :: [(Set.Set Char, Set.Set Char)] -> Integer
+sumOfCommonItemPrioritiesInHalves = sumOf (folded . folded . to priority) . fmap (uncurry Set.intersection)
+
+parseBackpacksInTriples :: String -> [[Set.Set Char]]
+parseBackpacksInTriples = chunksOf 3 . fmap Set.fromList . lines
+
+sumOfCommonItemPrioritiesInTriples :: [[Set.Set Char]] -> Integer
+sumOfCommonItemPrioritiesInTriples = sumOf (folded . folded . to priority) . fmap (foldr1 Set.intersection)
 
 run :: String -> IO ()
 run xs = do
-  print xs
-  let parsed = parse xs
-  print parsed
-  let resA = solveA parsed
+  let parsed = parseBackpacksInHalves xs
+  let resA = sumOfCommonItemPrioritiesInHalves parsed
   print resA
 
--- resA @=? 1715
--- let resB = solveB parsed
--- print resB
--- resB @=? 1739
+  resA @=? 8233
+
+  let parsed = parseBackpacksInTriples xs
+  let resB = sumOfCommonItemPrioritiesInTriples parsed
+  print resB
+
+  resB @=? 2821
