384d41246e33480549ca586018d521308dd19793
Morten Kolstad <morten.kolstad@bekk.no>
2022-12-09 14:26:48 +0100

    memoize

--- src/lib/Day/Day07.hs
+++ src/lib/Day/Day07.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Day.Day07 (run) where
 
-import Data.List
+import Data.Function.Memoize
+import Data.List (groupBy, isPrefixOf, sort, stripPrefix)
 import Data.List.Extra (groupBy, splitOn, stripPrefix, sumOn')
 import Data.Map (Map)
 import Data.Map qualified as Map
-
 import Test.HUnit ((@=?))
 
 data FileDir = File Int String | Dir String deriving (Show)
@@ -13,23 +15,25 @@ data Path' = DotDot | Outer | Specific String deriving (Show)
 
 data Execution = CD Path' | LS [FileDir] deriving (Show)
 
-data Path = Root | Path :/ String deriving (Eq, Ord)
+data Path = Root | Path :/ String deriving (Eq, Ord, Show)
+
+deriveMemoizable ''Path
 
 moveUp :: Path -> Path
 moveUp Root = Root
 moveUp (p :/ _) = p
 
 parse :: String -> [Execution]
-parse = fmap parseExec . groupBy f . lines
+parse = fmap parseExec . groupBy groupByDollar . lines
  where
-  f _ b = not (isPrefixOf "$" b)
+  groupByDollar _ b = not (isPrefixOf "$" b)
 
   parseExec [s]
     | Just path <- stripPrefix "$ cd " s =
         CD $ case path of
           "/" -> Outer
           ".." -> DotDot
-          _ -> Specific path
+          s -> Specific s
   parseExec ("$ ls" : rest) = LS $ fmap parseFileDir rest
   parseExec s = error $ show s
 
@@ -43,36 +47,39 @@ makeMap :: [Execution] -> Map Path [FileDir]
 makeMap = Map.fromList . getPaths' Root
  where
   getPaths' curPath [] = []
-  getPaths' curPath ((LS fds) : rest) = (curPath, fds) : getPaths' curPath rest
-  getPaths' curPath ((CD pa) : rest) = getPaths' newP rest
+  getPaths' curPath (LS fds : rest) = (curPath, fds) : getPaths' curPath rest
+  getPaths' curPath (CD pa : rest) = getPaths' newP rest
    where
     newP = case pa of
       DotDot -> moveUp curPath
       Outer -> Root
       Specific s -> curPath :/ s
 
-countSize :: Path -> Map Path [FileDir] -> Int
-countSize path m
-  | Just fd <- Map.lookup path m = sumOn' f fd
+-- the memoization doesn't seem to increase performance.
+-- I guess the lookup on Path (~[String]) is not optimal
+countSize :: Map Path [FileDir] -> Path -> Int
+countSize m = memoizedRec
  where
-  f (File i _) = i
-  f (Dir s) = countSize (path :/ s) m
-countSize _ _ = error ""
+  memoizedRec = memoize mem
+  mem path = sumOn' (f path) (m Map.! path)
+
+  f _ (File i _) = i
+  f pp (Dir s) = memoizedRec (pp :/ s)
 
 sumOfSmallDirs :: [Execution] -> Int
 sumOfSmallDirs = sum . filter (<= 100000) . getSizes . makeMap
  where
-  getSizes m = fmap (\path -> countSize path m) $ Map.keys m
+  getSizes m = fmap (countSize m) $ Map.keys m
 
-findSmallestDirSizeThatFreesSpace :: [Execution] -> Maybe Int
+findSmallestDirSizeThatFreesSpace :: [Execution] -> Int
 findSmallestDirSizeThatFreesSpace execs = getFirst sizes
  where
-  getFirst = find (\x -> x + targetMin >= 30000000)
+  getFirst = minimum . filter (\x -> x + targetMin >= 30000000)
   targetMin = 70000000 - maximum sizes
 
   fileMap = makeMap execs
 
-  sizes = sort $ fmap (\path -> countSize path fileMap) $ Map.keys fileMap
+  sizes = sort $ fmap (countSize fileMap) $ Map.keys fileMap
 
 run :: String -> IO ()
 run xs = do
@@ -80,9 +87,8 @@ run xs = do
 
   let resA = sumOfSmallDirs parsed
   print resA
-
   resA @=? 1118405
+
   let resB = findSmallestDirSizeThatFreesSpace parsed
   print resB
-
-  resB @=? Just 12545514
+  resB @=? 12545514
