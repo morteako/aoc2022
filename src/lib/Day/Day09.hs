3aee9f65cff64cc3764fd2250b8553af44ded892
Morten Kolstad <morten.kolstad@bekk.no>
2022-12-10 16:23:44 +0100

    simplify p1 andd with lists. close

--- src/lib/Day/Day09.hs
+++ src/lib/Day/Day09.hs
@@ -7,7 +7,8 @@ import Data.List.Extra
 import Debug.Trace
 import Linear
 import Test.HUnit ((@=?))
-import Utils
+import Utils (readInt)
+import Utils qualified
 
 data Dir = R | L | D | U deriving (Show, Read, Enum)
 
@@ -23,41 +24,41 @@ dirToVec L = V2 (-1) 0
 dirToVec D = V2 0 (-1)
 dirToVec U = V2 0 1
 
-isDia a b =
-  elem a $ map (b +) $ V2 <$> [1, (-1)] <*> [1, (-1)]
+-- isDia a b =
+--   elem a $ map (b +) $ V2 <$> [1, (-1)] <*> [1, (-1)]
 
-getWeirdLine target tail =
-  elem tail $ map (target +) $ map dirToVec [R .. U]
+-- getWeirdLine target tail =
+--   elem tail $ map (target +) $ map dirToVec [R .. U]
 
-getWeirdDiagonal target tail
-  | tail + V2 2 1 == target = Just $ tail + 1
-  | tail + V2 2 (-1) == target = Just $ tail + V2 1 (-1)
-  | tail + V2 (-2) (-1) == target = Just $ tail + V2 (-1) (-1)
-  | tail + V2 (-2) (1) == target = Just $ tail + V2 (-1) (1)
-getWeirdDiagonal _ _ = Nothing
+-- getWeirdDiagonal target tail
+--   | tail + V2 2 1 == target = Just $ tail + 1
+--   | tail + V2 2 (-1) == target = Just $ tail + V2 1 (-1)
+--   | tail + V2 (-2) (-1) == target = Just $ tail + V2 (-1) (-1)
+--   | tail + V2 (-2) (1) == target = Just $ tail + V2 (-1) (1)
+-- getWeirdDiagonal _ _ = Nothing
 
-doDirTwo [] ht = traceLab "_____" [ht]
-doDirTwo ((_ :> 0) : rest) ht = doDirTwo rest ht
-doDirTwo (dn : _) ht
-  | traceShow (dn, ht) False = undefined
-doDirTwo (d :> (subtract 1 -> n) : rest) [h, t] = [h, t] : doDirTwo (d :> n : rest) newHT
- where
-  dv = dirToVec d
+-- doDirTwo [] ht = traceLab "_____" [ht]
+-- doDirTwo ((_ :> 0) : rest) ht = doDirTwo rest ht
+-- doDirTwo (dn : _) ht
+--   | traceShow (dn, ht) False = undefined
+-- doDirTwo (d :> (subtract 1 -> n) : rest) [h, t] = [h, t] : doDirTwo (d :> n : rest) newHT
+--  where
+--   dv = dirToVec d
 
-  newHT = [h', t']
-  h' = h + dv
-  t' = getTail ()
+--   newHT = [h', t']
+--   h' = h + dv
+--   t' = getTail ()
 
-  getTail ()
-    | h == t = traceLab "start" t -- start
-    | h' == t = traceLab "overlap" t -- overlap
-    | getWeirdLine h' t = traceLab "weird" t -- weird
-    | isDia h' t = traceLab "newDirCase" t -- newDirCase
-    | h == t + dv = traceLab "normal case" h -- normal case
-    | isDia h t = traceLab "diagonal" h -- diagonal
-    -- \| hx' == tx || hy' == ty = t + d
-    | otherwise = error $ "hmmm " <> show (d, h, t)
-doDirTwo _ _ = undefined
+--   getTail ()
+--     | h == t = traceLab "start" t -- start
+--     | h' == t = traceLab "overlap" t -- overlap
+--     | getWeirdLine h' t = traceLab "weird" t -- weird
+--     | isDia h' t = traceLab "newDirCase" t -- newDirCase
+--     | h == t + dv = traceLab "normal case" h -- normal case
+--     | isDia h t = traceLab "diagonal" h -- diagonal
+--     -- \| hx' == tx || hy' == ty = t + d
+--     | otherwise = error $ "hmmm " <> show (d, h, t)
+-- doDirTwo _ _ = undefined
 
 data NDir
   = NL
@@ -68,7 +69,7 @@ data NDir
   | NUL
   | NDR
   | NDL
-  | Same
+  | Stay
   deriving (Show)
 
 getNDirVec :: NDir -> V2 Int
@@ -80,7 +81,7 @@ getNDirVec NUR = 1
 getNDirVec NUL = V2 (-1) 1
 getNDirVec NDR = V2 1 (-1)
 getNDirVec NDL = -1
-getNDirVec Same = 0
+getNDirVec Stay = 0
 
 toNDir :: Dir -> NDir
 toNDir R = NR
@@ -88,35 +89,73 @@ toNDir L = NL
 toNDir D = ND
 toNDir U = NU
 
-data Dot = V2 Int :# NDir deriving (Show)
+data Label = H | T | Lab Int
+
+instance Show Label where
+  show H = "H"
+  show T = "T"
+  show (Lab n) = show n
+
+data Dot = Dot Label (V2 Int) deriving (Show)
+
+getDotPos (Dot _ p) = p
+
+un = undefined
+
+debug = True
+
+traceLab s x = if debug then Utils.traceLab s x else x
 
 doDir [] ht = traceLab "_____" [ht]
 doDir ((_ :> 0) : rest) ht = doDir rest ht
 doDir (dn : _) ht
   | traceShow (dn, ht) False = undefined
-doDir (d :> (subtract 1 -> n) : rest) ht@[h :# _, tdot] = ht : doDir (d :> n : rest) newHT
+doDir (d :> (subtract 1 -> n) : rest) ht@[Dot hlab hpos, tdot] = ht : doDir (d :> n : rest) newHT
  where
-  dv = dirToVec d
+  moveDir = dirToVec d
 
-  newHT = [h' :# toNDir d, t']
-  h' = h + dv
-  t' = getTail h' tdot
+  newHT = [Dot hlab newHPos, t']
+  newHPos = hpos + moveDir
 
-  getTail h' (t :# ndir)
-    -- h' == t + dir * 2
-    -- \| h == t = traceLab "start" t :# Same -- start
-    | h' == t = traceLab "overlap" t :# Same -- overlapping
-    | getWeirdLine h' t = traceLab "weird" t -- weird
-    | isDia h' t = traceLab "newDirCase" t -- newDirCase
-    | h == t + getNDirVec ndir = traceLab "normal case" h -- normal case
-    | isDia h t = traceLab "diagonal" h -- diagonal
-    -- \| hx' == tx || hy' == ty = t + d
-    | otherwise = error $ "hmmm " <> show (d, h, t)
+  newH = Dot hlab newHPos
+
+  t' = getTail hpos newH tdot
+
+  getTail prevHead (Dot curHeadLab curHeadPos) tt@(Dot tlab tpos)
+    | curHeadPos == tpos = traceLab "same spot - stay" $ Dot tlab tpos
+    | isTouching curHeadPos tpos = traceLab "isTouching stay" $ Dot tlab tpos
+    | Just nextTPos <- isTwoDirAway curHeadPos tpos = traceLab "sameDir *2" $ Dot tlab nextTPos
+    | isDoubleDiag curHeadPos tpos = traceLab "doubleDiag usePrev" $ Dot tlab prevHead
+    | otherwise = error $ "hmmm " <> show (d, curHeadPos, tt)
 doDir _ _ = undefined
+
+a .+ dir = a + getNDirVec dir
+
+isTouching a b = case abs (a - b) of
+  V2 1 1 -> True
+  V2 1 0 -> True
+  V2 0 1 -> True
+  _ -> False
+
+isDoubleDiag a b = case abs (a - b) of
+  V2 2 1 -> True
+  V2 1 2 -> True
+  _ -> False
+
+isTwoDirAway a b = case (a - b) of
+  V2 2 0 -> Just $ a - V2 1 0
+  V2 (-2) 0 -> Just $ a + V2 1 0
+  V2 0 (-2) -> Just $ a + V2 0 1
+  V2 0 2 -> Just $ a - V2 0 1
+  _ -> Nothing
 
 -- \| view _x h ==
 
-solveA moves = length $ nub $ fmap last $ doDirTwo moves [0, 0]
+more f = length . nub . fmap f . fmap last
+
+-- solveA moves = more id $ doDirTwo moves [0, 0]
+
+solveA' moves = more getDotPos $ doDir moves [Dot H 0, Dot T 0]
 
 -- f !(cur : (traceLab "cd" -> cs)) (traceLab "f" -> dir) = doDir dir cur ++ cur : cs
 
@@ -126,17 +165,15 @@ run :: String -> IO ()
 run xs = do
   let parsed = parse xs
   print parsed
-  let resA = solveA parsed
   -- print resA
 
-  print $ solveA parsed
+  let resA = solveA' parsed
   -- mapM_ print $ groupOn (view _x) $ sort $ nub $ fmap snd $ solveA parsed
   -- print "--"
   -- mapM print $ sort $ fmap snd $ solveA parsed
 
-  resA @=? 6269
-  -- let resB = solveB parsed
-  -- print resB
-  -- resB @=? 1739
+  print resA
+
+  -- mapM_ print $ zip resA resA'
 
   pure ()
