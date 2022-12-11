de943f2505f2ef467294c49d5da70030068f4e4a
Morten Kolstad <morten.kolstad@bekk.no>
2022-12-11 02:42:56 +0100

    cleanup

--- src/lib/Day/Day09.hs
+++ src/lib/Day/Day09.hs
@@ -1,12 +1,10 @@
 module Day.Day09 (run) where
 
-import Control.Lens (view)
 import Data.Containers.ListUtils (nubOrd)
-import Data.Map qualified as Map
 import Linear (V2 (..))
 import Test.HUnit ((@=?))
-import Utils (readInt)
-import Utils qualified
+
+-- Horrible stuff 🙃
 
 data Dir = R | L | D | U deriving (Read, Enum, Show)
 
@@ -15,7 +13,7 @@ data Move = Dir :> Int deriving (Show)
 parseMoves :: String -> [Move]
 parseMoves = fmap parseMove . lines
  where
-  parseMove (words -> [l, readInt -> n]) = read @Dir l :> n
+  parseMove (words -> [l, read -> n]) = read l :> n
 
 dirToVec :: Num a => Dir -> V2 a
 dirToVec R = V2 1 0
@@ -23,49 +21,40 @@ dirToVec L = V2 (-1) 0
 dirToVec D = V2 0 (-1)
 dirToVec U = V2 0 1
 
-data Label = H | T | Lab Int deriving (Eq, Ord)
-
-instance Show Label where
-  show H = "H"
-  show T = "T"
-  show (Lab n) = show n
-
-data Dot = Dot Label (V2 Int)
-
-doDirNew :: [Move] -> [Dot] -> [[Dot]]
+doDirNew :: [Move] -> [V2 Int] -> [[V2 Int]]
 doDirNew [] ht = [ht]
 doDirNew ((_ :> 0) : rest) ht = doDirNew rest ht
-doDirNew ddd@(d :> (subtract 1 -> n) : rest) ht@(Dot hlab hpos : ts) = ht : doDirNew (d :> n : rest) newHT
+doDirNew (d :> (subtract 1 -> n) : rest) ht@(hpos : ts) = ht : doDirNew (d :> n : rest) newHT
  where
   newHPos = hpos + dirToVec d
-  newH = Dot hlab newHPos
+  newH = newHPos
 
   newHT = getTails hpos newH ts
 
-  getTails prevHead d@(Dot _ curHeadPos) [] = [d]
-  getTails prevHead d@(Dot _ curHeadPos) (tt@(Dot tlab tpos) : tts) =
+  getTails prevHead curHeadPos [] = [curHeadPos]
+  getTails prevHead curHeadPos (tpos : tts) =
     let res =
           if
               | newHPos == tpos ->
-                  Dot tlab tpos
+                  tpos
               | curHeadPos == tpos ->
-                  Dot tlab tpos
+                  tpos
               | prevHead == curHeadPos ->
-                  Dot tlab tpos
+                  tpos
               | prevHead == tpos ->
-                  Dot tlab tpos
+                  tpos
               | Just nextTPos <- isTwoDirAway curHeadPos tpos ->
-                  Dot tlab nextTPos
+                  nextTPos
               | isLineTouch curHeadPos tpos ->
-                  Dot tlab tpos
+                  tpos
               | Just dirV <- isDiag prevHead curHeadPos ->
-                  Dot tlab (tpos + dirV)
+                  tpos + dirV
               | isTouching curHeadPos tpos ->
-                  Dot tlab tpos
+                  tpos
               | isDoubleDiag curHeadPos tpos ->
-                  Dot tlab prevHead
+                  prevHead
               | otherwise -> undefined
-     in d : getTails tpos res tts
+     in curHeadPos : getTails tpos res tts
 doDirNew _ _ = undefined
 
 isDiag a b = case abs (a - b) of
@@ -97,13 +86,11 @@ isTwoDirAway a b = case (a - b) of
   V2 0 2 -> Just $ a - V2 0 1
   _ -> Nothing
 
-countLastTailPoses :: [[Dot]] -> Int
-countLastTailPoses = length . nubOrd . fmap getDotPos . fmap last
- where
-  getDotPos (Dot _ p) = p
+countLastTailPoses :: [[V2 Int]] -> Int
+countLastTailPoses = length . nubOrd . fmap last
 
 countTailPositions :: [Move] -> Int -> Int
-countTailPositions moves numTails = countLastTailPoses $ doDirNew moves $ [Dot H 0] ++ fmap (\x -> Dot (Lab x) 0) [1 .. numTails]
+countTailPositions moves numTails = countLastTailPoses $ doDirNew moves $ 0 : fmap (const 0) [1 .. numTails]
 
 run :: String -> IO ()
 run xs = do
