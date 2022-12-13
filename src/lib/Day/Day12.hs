3c6e183bfb7aa3bcc920b2ab3f9683837eff6490
Morten Kolstad <morten.kolstad@bekk.no>
2022-12-13 21:19:49 +0100

    working 12, but ugly

---
+++ src/lib/Day/Day12.hs
@@ -0,0 +1,98 @@
+module Day.Day12 (run) where
+
+import Control.Lens (
+  folded,
+  folding,
+  ifoldMapOf,
+  lined,
+  reindexed,
+  (<.),
+  (<.>),
+ )
+import Data.Graph.Inductive (Graph (mkGraph))
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Data.Graph.Inductive.Query (sp)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Maybe (mapMaybe)
+import Data.Tuple (swap)
+import Letter (Letter, toLetter)
+import Linear (V2 (..))
+import Test.HUnit ((@=?))
+
+type Point = V2 Int
+
+data S = Start | End | Char Char deriving (Show, Eq)
+
+getVal :: S -> Char
+getVal Start = 'a'
+getVal End = 'z'
+getVal (Char c) = c
+
+parseAsciiMap ::
+  (Char -> Maybe a) ->
+  String ->
+  Map Point a
+parseAsciiMap f = ifoldMapOf (asciiGrid <. folding f) Map.singleton
+ where
+  asciiGrid = reindexed (uncurry (flip V2)) (lined <.> folded)
+
+parse :: String -> Map Point S
+parse s = grid
+ where
+  grid = parseAsciiMap (Just . f) s
+  f 'S' = Start
+  f 'E' = End
+  f x = Char x
+
+get3 (_, _, x) = x
+
+toTup (x, y, _) = (x, y)
+
+data StartEnd = [Int] :=> Int
+
+makeGraph :: _ -> Map (V2 Int) _ -> (Gr (V2 Int) Int, _, _)
+makeGraph startSymbols grid =
+  ( mkGraph (fmap swap $ Map.toList nodeMap) (fmap makeLedge $ fmap toTup $ remNotValid $ concat $ Map.elems $ gridWithHeighDiffs)
+  , start :=> end
+  , nodeMap
+  )
+ where
+  charGrid = fmap getVal grid
+  remNotValid = filter ((<= 1) . get3)
+  gridWithHeighDiffs = Map.mapWithKey getHeighDiffs charGrid
+  getHeighDiffs k v = mapMaybe (\neigh -> (\nV -> diff k neigh v nV) <$> Map.lookup neigh charGrid) $ neighCoords k
+
+  diff k newK curV newV = (k, newK, diff' curV newV)
+  diff' b a = fromEnum a - fromEnum b
+
+  makeLedge (x, y) = (nodeMap Map.! x, nodeMap Map.! y, 1)
+
+  start' = Map.keys $ Map.filter (`elem` startSymbols) grid
+  (end', _) = Map.findMin $ Map.filter (== End) grid
+
+  start = (nodeMap Map.!) <$> start'
+  end = nodeMap Map.! end'
+
+  nodeMap = Map.fromList $ flip zip [0 ..] $ Map.keys charGrid
+
+neighCoords :: V2 Int -> [V2 Int]
+neighCoords v2 = (v2 +) <$> [V2 0 1, V2 1 0, V2 (-1) 0, V2 0 (-1)]
+
+getShortestPathtoEnd :: [S] -> Map (V2 Int) S -> Int
+getShortestPathtoEnd startSymbols grid = minimum $ fmap (pred . length) res
+ where
+  res = mapMaybe (\s -> sp s end g) starts
+  (g, starts :=> end, nodeMap) = makeGraph startSymbols grid
+
+run :: String -> IO ()
+run xs = do
+  let parsed = parse xs
+
+  let resA = getShortestPathtoEnd [Start] parsed
+  print $ resA
+  resA @=? 528
+
+  let resB = getShortestPathtoEnd [Start, Char 'a'] parsed
+  print resB
+  resB @=? 522
--- src/lib/Solutions.hs
+++ src/lib/Solutions.hs
@@ -13,6 +13,7 @@ import Day.Day08 qualified
 import Day.Day09 qualified
 import Day.Day10 qualified
 import Day.Day11 qualified
+import Day.Day12 qualified
 
 import Data.Map (Map)
 import Data.Map qualified as Map
@@ -35,4 +36,5 @@ solutions =
     , "09" =: Day.Day09.run
     , "10" =: Day.Day10.run
     , "11" =: Day.Day11.run
+    , "12" =: Day.Day12.run
     ]
