163bfd9bb44c76411067b8255cd4c5220b383750
Morten Kolstad <morten.kolstad@bekk.no>
2022-12-13 22:57:32 +0100

    day13 initial

--- src/lib/Solutions.hs
+++ src/lib/Solutions.hs
@@ -14,10 +14,11 @@ import Day.Day09 qualified
 import Day.Day10 qualified
 import Day.Day11 qualified
 import Day.Day12 qualified
+import Day.Day13 qualified
 
 import Data.Map (Map)
 import Data.Map qualified as Map
-import DayVersion (DayVersion (NormalDay, SpecialVersion))
+import DayVersion (DayVersion)
 import Utils ((=:))
 
 solutions :: Map DayVersion (String -> IO ())
@@ -37,4 +38,5 @@ solutions =
     , "10" =: Day.Day10.run
     , "11" =: Day.Day11.run
     , "12" =: Day.Day12.run
+    , "13" =: Day.Day13.run
     ]
