3694a75ae5c64a58de91379c37a21cc75f449218
Morten Kolstad <morten.kolstad@bekk.no>
2022-12-23 23:31:46 +0100

    33 sec?

--- src/lib/Day/Day23.hs
+++ src/lib/Day/Day23.hs
@@ -1,19 +1,18 @@
 module Day.Day23 (run) where
 
 import Control.Lens
-import Control.Monad
 import Data.Coerce
 import Data.Foldable
+import Data.List qualified as List
 import Data.Map (Map)
 import Data.Map qualified as Map hiding (null)
 import Data.Maybe
 import Data.Semigroup
-import Data.Set hiding (drop, take)
+import Data.Set (Set)
 import Data.Set qualified as Set hiding (drop, null, take)
-import Debug.Trace
-import Linear hiding (E, trace)
+
+import Linear (V2 (..))
 import Test.HUnit ((@=?))
-import Utils (printMap, traceLab)
 
 type Point = V2 Int
 
@@ -41,8 +40,6 @@ getDirV E = V2 1 0
 
 ords = [([N, W, E], N), ([S, W, E], S), ([N, S, W], W), ([N, S, E], E)]
 
-cycords = cycle ords
-
 cyc (flip mod 4 -> id -> n) xs =
   drop n $ take (4 + n) $ cycle xs
 
@@ -68,14 +65,6 @@ getNeighs' v@(V2 x y) = do
         then v + getDirV d
         else v + getDirV d + dv
 
--- findNewDir d m =
---   where
---     ns = getNeighs d m
-
--- t = (fromList [V2 0 2, V2 1 5, V2 2 0, V2 3 3, V2 4 1])
-
--- tt = move 3 (V2 2 5) (fromList [V2 1 2, V2 2 1, V2 2 5, V2 3 1, V2 4 3])
-
 move i v m = case getNeighs i v m of
   Nothing -> v
   Just d -> getDirV d + v
@@ -87,9 +76,6 @@ oneRound (i, m) = (i + 1, Map.foldMapWithKey f newPoses)
   f k [x] = Set.singleton k
   f _ xs = Set.fromList xs
 
-  -- newValidPos = Map.mapMaybe onlySingl newPoses
-  -- onlySingl [x] = Just x
-  -- onlySingl _ = Nothing
   newPoses = Map.unionsWith (++) $ foldMap (\k -> [Map.singleton (move i k m) [k]]) m
 
 getEdges :: Set (V2 Int) -> (Int, Int, Int, Int)
@@ -97,49 +83,25 @@ getEdges = coerce . foldMap f
  where
   f (V2 x y) = (Min x, Max x, Min y, Max y)
 
-makeMap s = foldMap (\v -> Map.singleton v True) s <> Map.fromList (fmap (,False) coords)
+solveA grid = rect - Set.size res
  where
-  coords = V2 <$> [ia .. ax] <*> [iy .. ay]
-  (ia, ax, iy, ay) = getEdges s
-
-solveA grid = (count, c, res) -- last $ take 11 $ iterate oneRound (0, grid)
- where
-  (c, res) = last $ take 11 $ iterate oneRound (0, grid)
-  i@(ia, ax, iy, ay) = getEdges res
+  (_, res) = last $ take 11 $ iterate oneRound (0, grid)
+  (ia, ax, iy, ay) = getEdges res
 
   rect = (1 + ax - ia) * (1 + ay - iy)
-  count = rect - Set.size res
 
-solveB grid = asum $ ps
+solveB grid = fmap succ $ List.findIndex (uncurry (==)) $ ps
  where
-  ps = (zipWith f <*> tail) $ iterate oneRound (0, grid)
-  f (_, xs) (i, ys) | xs == ys = Just i
-  f _ _ = Nothing
-
--- -- solveB = id
-
-toDots True = '#'
-toDots False = '.'
-
-prMap = printMap . fmap toDots . makeMap . snd
+  ps = (zip <*> tail) $ snd <$> iterate oneRound (0, grid)
 
 run :: String -> IO ()
 run xs = do
   let parsed = parse xs
 
   let resA = solveA parsed
-  -- mapM_ print resA
-
   print resA
+  resA @=? 3762
 
-  print "----"
-
-  -- print tt
-
-  -- mapM_ print $ fmap cyc [1 .. 10]
-
-  -- resA @=? 1715
   let resB = solveB parsed
   print resB
-
--- resB @=? 1739
+  resB @=? Just 997
